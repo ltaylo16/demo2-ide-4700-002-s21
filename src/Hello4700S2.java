8535bc762d5d6fe7fd461326222707763a77967c
ms-O <kristin.obermyer@villanova.edu>
2021-04-05 16:19:35 -0400

    Add another message

--- src/Hello4700S2.java
+++ src/Hello4700S2.java
@@ -3,6 +3,7 @@ public class Hello4700S2 {
 
     public static void main(String[] args) {
         System.out.println("Hello 4700 Section 002!");
+        System.out.println("Keeping Eclipse in sync with Git");
     }
 
 }
